c91a295ce7446fc9b33e1fd1e7e7cc627066f639
Nima Makhmali <nmkhmly790@gmail.com>
2025-05-27 02:51:37 -0700

    update

--- change_to_1min.py
+++ change_to_1min.py
@@ -1,27 +1,25 @@
 import pandas as pd
 import mplfinance as mpf
-import matplotlib.pyplot as plt
 
-import warnings
-warnings.filterwarnings("ignore")
 
-def draw_chart():
-    df = pd.read_csv("lbank_kbars.csv")
-    df['time'] = pd.to_datetime(df['time'])
-    df.set_index('time', inplace=True)
+df = pd.read_csv("lbank_kbars.csv")
 
-    candles = df.resample('1T').agg({
-        'open': 'first',
-        'high': 'max',
-        'low': 'min',
-        'close': 'last',
-        'volume': 'sum'
-    }).dropna()
+df['time'] = pd.to_datetime(df['time'])
+df.set_index('time', inplace=True)
 
-    # قبل از رسم جدید، تمام پنجره‌های قبلی رو ببند
-    plt.close("all")
-    mpf.plot(candles, type='candle', volume=True, style='charles', title='LBank 1-Min Candles')
-    print("✅ Chart plotted")
+candles = df.resample('1T').agg({
+    'open': 'first',
+    'high': 'max',
+    'low': 'min',
+    'close': 'last',
+    'volume': 'sum'
+}).dropna()
 
-if __name__ == "__main__":
-    draw_chart()
+
+candles.to_csv("lbank_1min_candles.csv")
+print(" Read lbank_1min_candles.csv")
+
+mpf.plot(candles, type='candle', volume=True, style='charles', title='LBank 1-Min Candles')
+
+
+#indexing 
